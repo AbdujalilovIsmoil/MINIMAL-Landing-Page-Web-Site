39fc203e5fcf9f49c5ecc8eed74b45b3ff447c64
= <ismoilbek20050421@gmail.com>
2022-08-14 16:34:57 +0500

    MINIMAL WEB-SITE

--- JS/index.js
+++ JS/index.js
@@ -1,17 +1,26 @@
-let faBars = document.querySelector(".fa-bars");
-let times = document.querySelector(".fa-times");
-let navbar = document.querySelector(".new__nav");
-let newLink = document.querySelectorAll(".new__nav__link");
+// JAVASCRIPT TO THE CODES
+window.addEventListener("DOMContentLoaded", () => {
+  let faBars = document.querySelector(".fa-bars");
+  let times = document.querySelector(".fa-times");
+  let navbar = document.querySelector(".new__nav");
+  let newLink = document.querySelectorAll(".new__nav__link");
 
-faBars.addEventListener("click",()=>{
-  navbar.style.left = "0";
-});
-times.addEventListener("click",()=>{
-  navbar.style.left = "-100%";
-});
-
-for(let i = 0; i < newLink.length; i++){
-  newLink[i].addEventListener("click",()=>{
-    navbar.style.left = "-100%";
+  faBars.addEventListener("click", () => {
+    navbar.classList.add("active");
   });
-};
+  times.addEventListener("click", () => {
+    navbar.classList.remove("active");
+  });
+
+  newLink.forEach((link) => {
+    link.addEventListener("click", () => {
+      navbar.classList.remove("active");
+    });
+  });
+
+  this.addEventListener("keydown", (e) => {
+    if (e.code == "Escape") {
+      navbar.classList.remove("active");
+    }
+  });
+});
